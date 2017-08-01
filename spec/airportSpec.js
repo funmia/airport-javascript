3e9aff4f40d3a17966057ced660a793b89826d89
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-01 16:31:05 +0100

    Checks weather conditions

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -3,33 +3,40 @@ describe('Airport', function() {
   var airport, plane, weather;
 
   beforeEach(function (){
-    // weather = jasmine.createSpy('weather')
-    weather = new Weather();
+    weather = jasmine.createSpyObj('weather', ['isStormy'])
     airport = new Airport(weather);
-    console.log(airport);
     plane = 'plane';
   });
 
-  describe('land', function() {
-    it('lands a plane at an airport', function() {
+  describe('good weather', function() {
+    beforeEach (function(){
+      weather.isStormy.and.returnValue(false);
+    });
+
+    it('lands a plane at an airport in good weather', function() {
       airport.land(plane);
       expect(airport.plane()).toEqual(plane);
     });
-  });
 
-  describe('takeOff', function() {
-    beforeEach(function () {
-      airport.land(plane);
-    });
     it('takes off a plane from an airport in good weather', function() {
-      spyOn(weather, "isStormy").and.returnValue(false);
+      airport.land(plane);
       airport.takeOff(plane);
       expect(airport.plane()).toEqual(null);
     });
+
+  });
+
+  describe('bad weather', function() {
+    beforeEach(function () {
+      weather.isStormy.and.returnValue(true);
+    });
+
+    it('does not land a plane at an airport in bad weather', function() {
+      expect(function() {airport.land(plane); }).toThrowError('cannot land during storm')
+    });
+
     it('stops a plane taking off in stormy weather', function(){
-      spyOn(weather, "isStormy").and.returnValue(true);
-      airport.takeOff(plane);
-      expect(airport.plane()).toEqual(plane);
+      expect(function() {airport.takeOff(plane); }).toThrowError('cannot takeoff during storm')
     });
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,6 @@
 function Airport(weather) {
   this._weather = weather;
+  this._plane = null;
 };
 
 Airport.prototype.plane = function() {
@@ -7,13 +8,17 @@ Airport.prototype.plane = function() {
 };
 
 Airport.prototype.land = function(plane) {
-  this._plane = plane
+  if (this._isWeatherStormy()) {
+    throw new Error('cannot land during storm');
+  }
+  this._plane = plane;
 };
 
 Airport.prototype.takeOff = function(plane) {
-  if (this._isWeatherStormy() === false) {
-    this._plane = null;
+  if (this._isWeatherStormy()) {
+    throw new Error('cannot takeoff during storm');
   }
+  this._plane = null;
 };
 
 Airport.prototype._isWeatherStormy = function() {
